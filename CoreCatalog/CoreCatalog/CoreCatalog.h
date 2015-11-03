70596dd3c389d751cc00814931f240d4922a70aa
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-11-03 03:06:46 -0500

    working on model

---
+++ CoreCatalog/CoreCatalog/CoreCatalog.h
@@ -0,0 +1,19 @@
+//
+//  CoreCatalog.h
+//  CoreCatalog
+//
+//  Created by Alsey Coleman Miller on 11/3/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreCatalog.
+FOUNDATION_EXPORT double CoreCatalogVersionNumber;
+
+//! Project version string for CoreCatalog.
+FOUNDATION_EXPORT const unsigned char CoreCatalogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCatalog/PublicHeader.h>
+
+
