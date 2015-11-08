2f6ceafbf9b447c4195372756425c9feaaa56f81
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-11-07 22:19:49 -0500

    added CatalogAdmin and CoreCatalog iOS target

---
+++ CatalogAdmin/CatalogAdmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatalogAdmin
+//
+//  Created by Alsey Coleman Miller on 11/7/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
